0e5441c493e97b9fa113ffcd73156ea1f3f193d8
Patrick <patrick.zebing.he@gmail.com>
2022-02-21 22:33:01 -0500

    Somewhat sandboxed

--- src/bot.py
+++ src/bot.py
@@ -5,11 +5,10 @@ import random
 import os
 from dotenv import load_dotenv
 import signal
-import sys
-from urllib.request import Request, urlopen
 import subprocess
 from threading import Timer
 import unshare
+
 load_dotenv()
 
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -30,10 +29,8 @@ def preexec():
     if os.name == "posix":
         unshare.unshare(unshare.CLONE_NEWNET)
         os.setuid(200)
-        
-    
 
-def compare(id, path, case):
+def compare(path, case):
     input_text = case[2]
     command = "python3"
     if os.name == "nt":
@@ -48,7 +45,6 @@ def compare(id, path, case):
         output = output[0].decode('utf-8')
         return output.strip().replace("\r\n", "\n") == case[3].strip()
     except Exception as ex:
-        print(ex)
         os.kill(os.getpid(k.pid), signal.SIGTERM)
         return False
     finally:
@@ -97,7 +93,7 @@ async def submitSolution(ctx, problem_id: int):
 
     for i,case in enumerate(cases):
         i += 1
-        if not compare(id,path,case):
+        if not compare(path, case):
             await ctx.send(f"Solution failed on test case: {i}")
             os.remove(path)
             return
@@ -106,57 +102,5 @@ async def submitSolution(ctx, problem_id: int):
     os.remove(path)
     await ctx.send("Solution passed all cases!")
 
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
 
 bot.run(DISCORD_BOT_TOKEN)
